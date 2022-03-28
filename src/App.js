7d247d282d88ae1f08cf955d4233020202350f87
Mingiian <khechievm08@yandex.ru>
2022-03-28 12:27:49 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "antd/dist/antd.css";
+import "./index.css";
+import { Table, Button } from "antd";
 
-function App() {
+const calsiusCalculated = (a) => {
+  const result = Math.round(a - 273);
+  if (result >= 1) return `${"+"}${result}`;
+  return result;
+};
+
+const fahrenheitCalculated = (a) => {
+  const result = Math.round(((a - 273.15) * 9) / 5 + 32);
+  if (result >= 1) return `${"+"}${result}`;
+  return result;
+};
+
+const App = () => {
+  const columns = [
+    {
+      title: "City",
+      dataIndex: "city",
+      key: "city",
+      width: "40%",
+    },
+    {
+      title: "°C",
+      dataIndex: "celsius",
+      key: "celsius",
+      width: "30%",
+      align: "center",
+    },
+    {
+      title: "℉",
+      dataIndex: "fahrenheit",
+      key: "fahrenheit",
+      width: "30%",
+      align: "center",
+    },
+  ];
+
+  const [data, setData] = useState([
+    {
+      key: "1",
+      city: "Moscow",
+      celsius: 0,
+      fahrenheit: 0,
+    },
+    {
+      key: "2",
+      city: "Saint Petersburg",
+      celsius: 0,
+      fahrenheit: 0,
+    },
+  ]);
+  const [open, setOpen] = useState(null);
+
+  const requestWeather = (town) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=9a6698e1ac53c8e8acfbf4d601a2aa26`
+    )
+      .then((res) => res.json())
+      .then((body) =>
+        setData((prevState) =>
+          prevState.map((el) =>
+            el.city === town
+              ? {
+                  ...el,
+                  city: body.name,
+                  celsius: calsiusCalculated(body.main.temp),
+                  fahrenheit: fahrenheitCalculated(body.main.temp),
+                }
+              : el
+          )
+        )
+      )
+      .catch((e) => console.log(e));
+  };
+
+  useEffect(() => {
+    if (open) {
+      requestWeather("Moscow");
+      requestWeather("Saint Petersburg");
+    }
+  }, [open]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Metcast</h1>
+      <Table columns={columns} dataSource={data} pagination={false} />
+      <Button
+        type="primary"
+        className="updateWeather"
+        ghost
+        onClick={() => setOpen(open + 0.01)}
+      >
+        Обновить
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
